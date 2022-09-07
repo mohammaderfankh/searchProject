4ca0730af54b2e81e8af1f8d72023200b0422217
mohammaderfankh <79629565+mohammaderfankh@users.noreply.github.com>
2022-09-07 14:08:14 +0430

    dynamic project & add btns script

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const searchInput = document.querySelector(".search");
+const productsDom = document.querySelector(".product-center");
+const btns = document.querySelectorAll(".btn");
 
 let allProductsData = [];
 
@@ -23,9 +25,30 @@ function renderProducts(_products, _filters) {
     const filteredProducts = _products.filter((p) => {
         return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
     });
+    productsDom.innerHTML = "";
+    filteredProducts.forEach((item, index) => {
+        const productsDiv = document.createElement("div");
+        productsDiv.classList.add("product");
+        productsDiv.innerHTML = `<div class="img-container">
+        <img src=${item.image} alt="p-${index}" />
+    </div>
+    <div class="caption">
+        <div class="price-caption">${item.price} $</div>
+        <div class="name-caption">${item.title}</div>
+    </div>`;
+        productsDom.appendChild(productsDiv);
+    });
 }
 
 searchInput.addEventListener("input", (e) => {
     filters.searchItems = e.target.value;
     renderProducts(allProductsData, filters);
+});
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        const filter = e.target.dataset.filter;
+        filters.searchItems = filter;
+        renderProducts(allProductsData, filters);
+    });
 });
